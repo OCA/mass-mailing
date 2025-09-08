OCA/social

[FIX] mass_mailing_event: Integration tests
When running tests and `mass_mailing_list_dynamic` was installed, they failed.

mass_mailing_event/tests/test_mass_mailing_event.py
@@ -3,10 +3,12 @@
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import common
+from odoo.tests.common import at_install, post_install, SavepointCase
 
 
-class TestMassMailingEvent(common.SavepointCase):
+@at_install(True)
+@post_install(True)
+class TestMassMailingEvent(SavepointCase):
 
     @classmethod
     def setUpClass(cls):

OCA/mass-mailing

mass_mailing_event_registration_exclude/tests/test_mass_mailing_event.py
@@ -3,10 +3,12 @@
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import common
+from odoo.tests.common import at_install, post_install, SavepointCase
 
 
-class TestMassMailingEvent(common.SavepointCase):
+@at_install(False)
+@post_install(True)
+class TestMassMailingEvent(SavepointCase):
 
     @classmethod
     def setUpClass(cls):